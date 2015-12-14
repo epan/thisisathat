62c3fe7452e126d39e579c23eda29f71af00777c
Eric Pan <ericpanbot@gmail.com>
2015-12-13 17:53:27 -0800

    Add local port and feedback

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.set('port', (process.env.PORT || 5000));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -26,6 +27,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
