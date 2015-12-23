0f1d1effc017ae56fde78b80eaba251cf4859279
Eric Pan <ericpanbot@gmail.com>
2015-12-22 22:50:29 -0800

    Clean out express

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var express = require('express');
-var app = express();
 var Twitter = require('twitter');
 
 var client = new Twitter({
@@ -11,63 +9,48 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-// Set ports in Heroku and locally
-app.set('port', (process.env.PORT || 5000));
+// Hardcoded list of possible nouns
+var NOUNS = [
+  'cat',
+  'dog',
+  'mouse',
+  'house'
+];
+var metaphor = '';
 
-// Metaphor generator middelware
-var randomMetaphor = function (req, res, next) {
-  // Hardcoded list of possible nouns
-  var nouns = [
-    'cat',
-    'dog',
-    'mouse',
-    'house'
-  ];
-  var metaphor = '';
+// Returns a random integer between min (included) and max (excluded)
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
-  // Returns a random integer between min (included) and max (excluded)
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+// Capitalizes the first letter of a string
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-  // Capitalizes the first letter of a string
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+// Randomly select two nouns and generate a sentence
+function constructMetaphor(nouns) {
+  var firstNoun = '';
+  var secondNoun = '';
 
-  // Randomly select two nouns and generate a sentence
-  function constructMetaphor(nouns) {
-    var firstNoun = '';
-    var secondNoun = '';
+  var numberOfNouns = nouns.length;
+  var nounIndex1 = 0;
+  var nounIndex2 = 0;
 
-    var numberOfNouns = nouns.length;
-    var nounIndex1 = 0;
-    var nounIndex2 = 0;
+  nounIndex1 = getRandomInt(0, numberOfNouns);
+  nounIndex2 = getRandomInt(0, numberOfNouns);
 
-    nounIndex1 = getRandomInt(0, numberOfNouns);
-    nounIndex2 = getRandomInt(0, numberOfNouns);
+  firstNoun = capitalizeFirstLetter(nouns[nounIndex1]);
+  secondNoun = nouns[nounIndex2];
 
-    firstNoun = capitalizeFirstLetter(nouns[nounIndex1]);
-    secondNoun = nouns[nounIndex2];
+  metaphor = firstNoun + ' is a ' + secondNoun + '.';
+  console.log(metaphor);
+}
 
-    metaphor = firstNoun + ' is a ' + secondNoun + '.';
-    console.log(metaphor);
-  }
-
-  return constructMetaphor(nouns);
-
-  next();
+constructMetaphor(NOUNS);
 
   // TODO: Figure out how to reference a file for the list of nouns externally
   // TODO: Prevent same noun from being selected twice
   // TODO: Figure out what to do for plural nouns for "are" instead of "is"
-  // TODO: Figure out how to deal with "is an" instead of "is a" // TODO: Make post to Twitter }; // Posts a tweet var postTweet = function (tweetText) { client.post('statuses/update', {status: tweetText},  function(error, tweet, response) { if(error) { throw error; } console.log(tweet);  // Tweet body. }); }; // Load the middleware
-app.use(postTweet(randomMetaphor()));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+  // TODO: Figure out how to deal with "is an" instead of "is a"
+  // TODO: Make post to Twitter
